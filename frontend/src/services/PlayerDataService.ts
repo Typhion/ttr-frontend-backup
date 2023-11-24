3348aa8f5a1ddcea79bcea385559ce29fa1a1e81
PuimegeT <thibault.puimege@student.kdg.be>
2023-11-24 14:30:00 +0100

    Now refetches after completing turn

--- frontend/src/services/PlayerDataService.ts
+++ frontend/src/services/PlayerDataService.ts
@@ -5,6 +5,14 @@ export type RandomWagonCardPick = {
     boardId: string;
 }
 export const pickRandomWagonCard = async (randomWagonCardPick: RandomWagonCardPick): Promise<void> => {
-    console.log("pickRandomWagonCard")
     await axios.post(`/player/pick/wagoncard/random`, randomWagonCardPick);
+}
+
+export type FaceUpWagonCardPick = {
+    playerId: string;
+    boardId: string;
+    wagonColor: string;
+}
+export const pickFaceUpWagonCard = async (faceUpWagonCardPick: FaceUpWagonCardPick): Promise<void> => {
+    await axios.post(`/player/pick/wagoncard/faceup`, faceUpWagonCardPick);
 }
