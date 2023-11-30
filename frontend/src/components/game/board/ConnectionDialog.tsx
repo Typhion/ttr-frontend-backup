c8a3f44be2c5c2908762b6f6497dd1ff1c0f076e
Rik Vijgen <rik.vijgen@student.kdg.be>
2023-12-01 00:56:45 +0100

    Fixed dialog not closing when claiming ferry or train connection

--- frontend/src/components/game/board/ConnectionDialog.tsx
+++ frontend/src/components/game/board/ConnectionDialog.tsx
@@ -102,6 +102,7 @@ export default function ConnectionDialog({
                 onSuccess: () => {
                     setSelectedIndices([])
                     refetch();
+                    onClose()
                 }
             });
     };
