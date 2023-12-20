abbff1c3667754cf00b360d5017a725d4491d5cd
Cedric Pollefeys <cedric.pollefeys@student.kdg.be>
2023-12-20 22:53:17 +0100

    profile has update for avatar

--- frontend/src/components/profile/Profile.tsx
+++ frontend/src/components/profile/Profile.tsx
@@ -1,22 +1,41 @@
 import {useNavigate, useParams} from "react-router-dom";
 import Button from "@mui/material/Button";
-import {Alert, Grid, Typography} from "@mui/material";
+import {Alert, Avatar, Box, Grid, Typography} from "@mui/material";
 import {useProfile} from "../../hooks/useProfile.ts";
 import Loader from "../general/Loader.tsx";
+import {getAvatarImage} from "../../model/Profile.ts";
+import PersonIcon from "@mui/icons-material/Person";
+import {useGetUnlockedAvatars} from "../../hooks/useGetUnlockedAvatars.ts";
+import {useChangeAvatar} from "../../hooks/useChangeAvatar.ts";
 
 export default function Profile() {
     const {uuid} = useParams<{ uuid: string }>();
-    const {isLoading: isLoading, isError: isError, data: profile} = useProfile(uuid);
+    const {isLoading: isLoading, isError: isError, data: profile, refetch } = useProfile(uuid);
+    const {isLoading: isLoadingAvatars, isError: isErrorAvatars, data: unlockedAvatars} = useGetUnlockedAvatars();
+    const changeAvatar = useChangeAvatar(
+        () => {
+            refetch();
+        }
+    );
     const navigate = useNavigate();
 
-    if (isLoading) return <Loader>Loading profile...</Loader>;
+    if (isLoading || isLoadingAvatars) return <Loader>Loading profile...</Loader>;
 
-    if (isError) {
+    if (isError || isErrorAvatars) {
         return <Alert severity="error">Unable to load profile.</Alert>;
     }
 
     const handleNavFriends = () => {
         navigate(`/profile/friends`);
+    }
+
+    const handleAvatarChange = (avatarId: string) => {
+        changeAvatar.mutate(avatarId);
+    }
+
+    let avatarImage: string | null = null;
+    if (profile?.avatar?.image) {
+        avatarImage = getAvatarImage(profile.avatar.image);
     }
 
     return (
@@ -54,8 +73,16 @@ export default function Profile() {
                           }}
                     >
                         <Typography variant="h5">Profile</Typography>
+                        <Avatar sx={{ width: 50, height: 50 }}>
+                            {avatarImage ? (
+                                <Avatar src={avatarImage} alt="Profile" sx={{ width: '100%', height: '100%' }} />
+                            ) : (
+                                <PersonIcon sx={{ fontSize: '200%' }} />
+                            )}
+                        </Avatar>
                         <Typography variant="body1">{profile.username}</Typography>
                     </Grid>
+
                     <Grid item xs={12} md={8}
                           style={{
                               border: '1px solid black',
@@ -74,6 +101,34 @@ export default function Profile() {
                             <Typography variant="h5">Score</Typography>
                             <Typography variant="body1">{profile.stats.totalScore} score</Typography>
                         </Grid>
+                    </Grid>
+
+                    <Grid item xs={12} md={4}
+                          style={{
+                              border: '1px solid black',
+                              borderRadius: '5px',
+                              padding: '10px',
+                              display: 'flex',
+                          }}
+                          container
+                    >
+                        <Grid item xs={12}>
+                            <Typography variant="h5">Unlocked Avatars</Typography>
+                        </Grid>
+                        {unlockedAvatars && unlockedAvatars.map((avatar, index) => (
+                            <Box key={index}>
+                                <Avatar
+                                    sx={{ width: 75, height: 75, margin: '5px', cursor: 'pointer' }}
+                                    onClick={() => handleAvatarChange(avatar.id)}
+                                >
+                                    {getAvatarImage(avatar.image) ? (
+                                        <Avatar src={getAvatarImage(avatar.image)!} alt="Profile" sx={{ width: '100%', height: '100%' }} />
+                                    ) : (
+                                        <PersonIcon sx={{ fontSize: '200%' }} />
+                                    )}
+                                </Avatar>
+                            </Box>
+                        ))}
                     </Grid>
                 </Grid>
             )}
--- frontend/src/model/Profile.ts
+++ frontend/src/model/Profile.ts
@@ -1,11 +1,43 @@
+import AvatarBlue from "../assets/images/avatars/bluepfp.png";
+import AvatarGreen from "../assets/images/avatars/greenpfp.png";
+import AvatarPurple from "../assets/images/avatars/purplepfp.png";
+import AvatarRed from "../assets/images/avatars/redpfp.png";
+import AvatarYellow from "../assets/images/avatars/yellowpfp.png";
+
+
+interface AvatarImages {
+    [key: string]: string;
+}
+const avatarImages: AvatarImages = {
+    bluepfp: AvatarBlue,
+    greenpfp: AvatarGreen,
+    purplepfp: AvatarPurple,
+    redpfp: AvatarRed,
+    yellowpfp: AvatarYellow,
+};
+
 export type Profile = {
     id: string;
     username: string;
     stats: ProfileStats;
+    avatar: Avatar;
 }
 
 export type ProfileStats = {
     gamesPlayed: number;
     gamesWon: number;
     totalScore: number;
+}
+
+export type Avatar = {
+    id: string;
+    name: string;
+    image: string;
+}
+
+export function getAvatarImage(imageId: string | undefined): string | null {
+    if (imageId === undefined) {
+        return null;
+    }
+    return avatarImages[imageId];
 }
