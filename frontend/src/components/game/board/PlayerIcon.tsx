87ef8535831e997faa19aad6828469548d4317e8
Rik Vijgen <rik.vijgen@student.kdg.be>
2024-01-03 15:12:46 +0100

    Players can now see their avatar in game

--- frontend/src/components/game/board/PlayerIcon.tsx
+++ frontend/src/components/game/board/PlayerIcon.tsx
@@ -1,6 +1,7 @@
 import {PlayerState} from "../../../model/GameState.ts";
-import {Avatar, Badge, Box, Typography} from "@mui/material";
-import PersonIcon from '@mui/icons-material/Person';
+import {Alert, Avatar, Badge, Box, Typography} from "@mui/material";
+import {useGetPlayerAvatar} from "../../../hooks/userHooks/useGetPlayerAvatar.ts";
+import Loader from "../../general/Loader.tsx";
 
 interface PlayerIconProps {
     playerState: PlayerState;
@@ -8,25 +9,30 @@ interface PlayerIconProps {
 
 export default function PlayerIcon({playerState}: PlayerIconProps) {
     const {score, wagonAmount, stationAmount} = playerState;
+    const {isLoading, isError,data: playerAvatar} = useGetPlayerAvatar(playerState.applicationUserId);
 
+    if (isLoading) return <Loader>Loading Player Avatar...</Loader>;
+    if (isError || !playerAvatar) return <Alert severity="error">Unable to load this player's avatar.</Alert>;
+
+    const avatarImage = `/src/assets/images/avatars/${playerAvatar.image}.png`;
     return (
         <Box sx={{
             position: 'relative',
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'center',
-            border: `5px solid ${playerState.color.toLowerCase()}`, //should be the player color eventually
+            border: `5px solid ${playerState.color.toLowerCase()}`,
             borderRadius: '50%',
             width: '6vw',
             height: '6vw',
         }}>
             <Avatar sx={{width: '100%', height: '100%'}}>
-                <PersonIcon sx={{fontSize: '200%'}}/>
+                <img src={avatarImage} alt={`Avatar of ${playerState.username}`} style={{ width: '100%', height: '100%', borderRadius: '50%' }} />
             </Avatar>
             <Badge
                 anchorOrigin={{vertical: 'top', horizontal: 'right'}}
                 badgeContent={<Typography variant="caption"
-                                          sx={{fontSize: '0.8em'}}>{score}</Typography>} // Font size in em for relative sizing
+                                          sx={{fontSize: '0.8em'}}>{score}</Typography>}
                 color="primary"
                 sx={{
                     position: 'absolute',
