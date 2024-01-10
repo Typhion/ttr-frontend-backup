b43995340ba13e86c1e3627bce025f4b05fb92b3
Frédéric Van Aken <frederic.vanaken@student.kdg.be>
2024-01-10 14:07:06 +0100

    small bugfix for HeaderAvatar - made avatars actually work in a game

---
+++ frontend/src/assets/images/avatars/index.ts
@@ -0,0 +1,15 @@
+import BluePfp from './bluepfp.png';
+import GreenPfp from './greenpfp.png';
+import PurplePfp from './purplepfp.png';
+import RedPfp from './redpfp.png';
+import YellowPfp from './yellowpfp.png';
+
+const AvatarImages: { [key: string]: string } = {
+    bluepfp: BluePfp,
+    greenpfp: GreenPfp,
+    purplepfp: PurplePfp,
+    redpfp: RedPfp,
+    yellowpfp: YellowPfp
+};
+
+export default AvatarImages;
